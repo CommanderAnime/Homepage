e4977d35ae2c7343afea50323c80eba1e1f542a7
Commander <asger.finding@gmail.com>
2022-01-10 10:15:43 +0100

    refactor(main): remove "Why are you reading this"

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,7 @@
+'use strict';
 import Navigation from './modules/Navigation.js';
 
 (() => {
-    console.log('%cHello! Why are you reading this?', 'font-size: 20px;');
-
     const navigation = new Navigation();
     $('body').prepend(navigation.body) && navigation.init();
 })();
