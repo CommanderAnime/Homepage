895c1499c6d9f68054cf07adf1fb0b136ea1a232
Commander <asger.finding@gmail.com>
2022-01-10 10:20:57 +0100

    feat(main): log page load

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,10 @@
 'use strict';
 import Navigation from './modules/Navigation.js';
+import Logger from './utils/Logger.js';
 
 (() => {
     const navigation = new Navigation();
     $('body').prepend(navigation.body) && navigation.init();
+
+    Logger.log('Site loaded')
 })();
